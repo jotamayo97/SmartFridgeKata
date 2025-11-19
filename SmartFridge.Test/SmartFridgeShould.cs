0b40c3c340167d8fde4568ce5e71a5d25f2f3345
Javier Fernández Mayo <jotamayo97@gmail.com>
2025-11-19 12:30:08 +0100

    fix acceptance test

--- SmartFridge.Test/SmartFridgeShould.cs
+++ SmartFridge.Test/SmartFridgeShould.cs
@@ -5,43 +5,28 @@ namespace SmartFridge.Test;
 public class SmartFridgeShould
 {
     [Fact]
-    public void Acceptance_scenario()
+    public void medium_acceptance_with_multiple_items_and_expired()
     {
-        var fridge = new SmartFridge(new DateTime(2021, 10, 18, 0, 0, 0), new EventManager());
+        var fridge = new SmartFridge(new DateTime(2021, 10, 18), new EventManager());
 
         fridge.FridgeDoorOpened();
-        fridge.ItemAdded("Milk",    new DateTime(2021, 10, 21, 0, 0, 0), ItemState.Sealed);
-        fridge.ItemAdded("Cheese",  new DateTime(2021, 11, 18, 0, 0, 0), ItemState.Sealed);
-        fridge.ItemAdded("Beef",    new DateTime(2021, 10, 20, 0, 0, 0), ItemState.Sealed);
-        fridge.ItemAdded("Lettuce", new DateTime(2021, 10, 22, 0, 0, 0), ItemState.Sealed);
+        fridge.ItemAdded("Milk",    new DateTime(2021,10,19), ItemState.Sealed);
+        fridge.ItemAdded("Cheese",  new DateTime(2021,10,25), ItemState.Sealed);
+        fridge.ItemAdded("Beef",    new DateTime(2021,10,21), ItemState.Opened);
+        fridge.ItemAdded("Lettuce", new DateTime(2021,10,20), ItemState.Sealed);
+        fridge.FridgeDoorClosed();
+
+        fridge.FridgeDoorOpened();
+        fridge.FridgeDoorClosed();
+
+        fridge.FridgeDoorOpened();
         fridge.FridgeDoorClosed();
 
         fridge.DayOver();
 
         fridge.FridgeDoorOpened();
-        fridge.FridgeDoorClosed();
-
-        fridge.FridgeDoorOpened();
-        fridge.FridgeDoorClosed();
-
-        fridge.FridgeDoorOpened();
-        fridge.ItemRemoved("Milk");
-        fridge.FridgeDoorClosed();
-
-        fridge.FridgeDoorOpened();
-        fridge.ItemAdded("Milk",    new DateTime(2021, 10, 26, 0, 0, 0), ItemState.Opened);
-        fridge.ItemAdded("Peppers", new DateTime(2021, 10, 23, 0, 0, 0), ItemState.Opened);
-        fridge.FridgeDoorClosed();
-
-        fridge.DayOver();
-
-        fridge.FridgeDoorOpened();
-        fridge.ItemRemoved("Beef");
         fridge.ItemRemoved("Lettuce");
-        fridge.FridgeDoorClosed();
-
-        fridge.FridgeDoorOpened();
-        fridge.ItemAdded("Lettuce", new DateTime(2021, 10, 22, 0, 0, 0), ItemState.Opened);
+        fridge.ItemAdded("Lettuce", new DateTime(2021,10,21), ItemState.Opened);
         fridge.FridgeDoorClosed();
 
         fridge.FridgeDoorOpened();
@@ -49,15 +34,16 @@ public class SmartFridgeShould
 
         fridge.DayOver();
 
-        String display = fridge.Display();
+        var display = fridge.Display();
 
-        Assert.Equal(
-            @"EXPIRED: Milk
-            Lettuce: 0 days remaining
-            Peppers: 1 day remaining
-            Cheese: 31 days remaining",
-            display);
-    
+        var expected =
+            "EXPIRED: Milk\n" +
+            "Beef: 0 days remaining\n" +
+            "Lettuce: 1 day remaining\n" +
+            "Cheese: 4 days remaining";
+
+
+        Assert.Equal(expected, display);
     }
     
     [Fact]
