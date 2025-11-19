79dfb34c468a90a8943806e889ba73c64d087a41
Javier Fernández Mayo <jotamayo97@gmail.com>
2025-11-19 09:42:26 +0100

    first commmit: aceptance test failing

---
+++ SmartFridge.Test/SmartFridgeShould.cs
@@ -0,0 +1,61 @@
+﻿namespace SmartFridge.Test;
+
+public class SmartFridgeShould
+{
+    [Fact]
+    public void Acceptance_scenario()
+    {
+        var fridge = new SmartFridge();
+        fridge.SetCurrentDate("18/10/2021");
+
+        fridge.FridgeDoorOpened();
+        fridge.ItemAdded("Milk",    "21/10/21", "sealed");
+        fridge.ItemAdded("Cheese",  "18/11/21", "sealed");
+        fridge.ItemAdded("Beef",    "20/10/21", "sealed");
+        fridge.ItemAdded("Lettuce", "22/10/21", "sealed");
+        fridge.FridgeDoorClosed();
+
+        fridge.DayOver();
+
+        fridge.FridgeDoorOpened();
+        fridge.FridgeDoorClosed();
+
+        fridge.FridgeDoorOpened();
+        fridge.FridgeDoorClosed();
+
+        fridge.FridgeDoorOpened();
+        fridge.ItemRemoved("Milk");
+        fridge.FridgeDoorClosed();
+
+        fridge.FridgeDoorOpened();
+        fridge.ItemAdded("Milk",    "26/10/21", "opened");
+        fridge.ItemAdded("Peppers", "23/10/21", "opened");
+        fridge.FridgeDoorClosed();
+
+        fridge.DayOver();
+
+        fridge.FridgeDoorOpened();
+        fridge.ItemRemoved("Beef");
+        fridge.ItemRemoved("Lettuce");
+        fridge.FridgeDoorClosed();
+
+        fridge.FridgeDoorOpened();
+        fridge.ItemAdded("Lettuce", "22/10/21", "opened");
+        fridge.FridgeDoorClosed();
+
+        fridge.FridgeDoorOpened();
+        fridge.FridgeDoorClosed();
+
+        fridge.DayOver();
+
+        String display = fridge.Display();
+
+        Assert.Equal(
+            @"EXPIRED: Milk
+            Lettuce: 0 days remaining
+            Peppers: 1 day remaining
+            Cheese: 31 days remaining",
+            display);
+    
+    }
+}
---
+++ SmartFridge/SmartFridge.cs
@@ -0,0 +1,39 @@
+﻿namespace SmartFridge;
+
+public class SmartFridge
+{
+    public void SetCurrentDate(string s)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FridgeDoorOpened()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ItemAdded(string beef, string p1, string @sealed)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FridgeDoorClosed()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DayOver()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ItemRemoved(string milk)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Display()
+    {
+        throw new NotImplementedException();
+    }
+}
